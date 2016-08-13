bc8886fc1e2f367140fc14ceef62c77eafa23630
Winston Wong <Winston Wong>
2016-08-13 19:57:24 -0400

    changed settings, included registration in INSTALLED_APPS

--- partyfood/settings.py
+++ partyfood/settings.py
@@ -188,5 +188,5 @@ AUTH_USER_MODEL = 'django_facebook.FacebookCustomUser'
 
 AUTH_PROFILE_MODULE = 'django_facebook.FacebookProfile'
 
-FACEBOOK_REGISTRATION_BACKEND = 'registration.backends.default.DefaultBackend'
+
 

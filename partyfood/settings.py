313244cf623f3b795d08b665a7a5d37b3afc7e30
Winston Wong <Winston Wong>
2016-08-12 23:24:05 -0400

    commit

--- partyfood/settings.py
+++ partyfood/settings.py
@@ -87,19 +87,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.i18n',
+                'django.core.context_processors.media',
+                'django.core.context_processors.static',
+                'django.core.context_processors.tz',
+                'django_facebook.context_processors.facebook',
             ],
-        },
-        'CONTEXT_PROCESSOR':(
-            'django.contrib.auth.context_processors.auth',
-            'django.core.context_processors.debug',
-            'django.core.context_processors.i18n',
-            'django.core.context_processors.media',
-            'django.core.context_processors.static',
-            'django.core.context_processors.tz',
-            'django.core.context_processors.request',
-            'django.contrib.messages.context_processors.messages',
-            'django_facebook.context_processors.facebook',
-        )
+        }, 
     },
 ]
 
